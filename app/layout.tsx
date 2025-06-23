6c24c55c9020576c6722abb3b6e99220582898ec
vedantkasargod <vedantkasargod.09@gmail.com>
2025-06-23 13:13:40 +0530

    New logo 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo_2.jpg" />
+        <link rel="icon" href="/logo_2.jpeg" />
       </head>
       <body>{children}</body>
     </html>
